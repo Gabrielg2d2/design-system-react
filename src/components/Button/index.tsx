406f3800ad55c1225e97830af53c6d6cb9bc2e93
Gabriel-g2d2 <gabriel.g2d2@gmail.com>
2022-10-12 22:55:26 -0300

    refactor: props button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,10 +1,14 @@
-export interface ButtonProps {
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
 }
 
-export function Button({ children }: ButtonProps) {
+export function Button({ children, ...rest }: ButtonProps) {
   return (
-    <button className="py-4 px-3 bg-cyan-500 rounded text-black text-sm w-full transition-colors hover:bg-cyan-300 focus:ring-2 ring-white">
+    <button
+      className="py-4 px-3 bg-cyan-500 rounded text-black text-sm w-full transition-colors hover:bg-cyan-300 focus:ring-2 ring-white"
+      {...rest}
+    >
       {children}
     </button>
   );
