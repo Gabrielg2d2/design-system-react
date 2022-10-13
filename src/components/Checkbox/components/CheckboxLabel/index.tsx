54d567a0be13bd492928fa256c66c9f7c4c14739
Gabriel-g2d2 <gabriel.g2d2@gmail.com>
2022-10-13 20:52:14 -0300

    refactor: gap label checkbox

--- src/components/Checkbox/components/CheckboxLabel/index.tsx
+++ src/components/Checkbox/components/CheckboxLabel/index.tsx
@@ -7,9 +7,9 @@ export function CheckboxLabel({ label, children }: CheckboxLabelProps) {
   if (!label) return <>{children}</>;
 
   return (
-    <label className="cursor-pointer flex items-center text-gray-100">
+    <label className="cursor-pointer flex items-center text-gray-100 gap-2">
       {children}
-      <span className="ml-2 text-ellipsis">{label}</span>
+      <span className="text-ellipsis">{label}</span>
     </label>
   );
 }
