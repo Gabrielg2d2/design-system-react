a6de662a07c975500a6fef17211f4b3ce5c4fab2
Gabriel-g2d2 <gabriel.g2d2@gmail.com>
2022-10-13 22:15:59 -0300

    refactor: props component text

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -2,6 +2,7 @@ export interface TextProps {
   children: React.ReactNode;
   size?: "sm" | "md" | "lg";
   component?: "p" | "span" | "div";
+  className?: string;
 }
 
 function color(size: string) {
@@ -15,11 +16,18 @@ function color(size: string) {
   }
 }
 
-export function Text({ children, size = "md", component = "span" }: TextProps) {
+export function Text({
+  children,
+  size = "md",
+  component = "span",
+  className,
+}: TextProps) {
   const Component = component as keyof JSX.IntrinsicElements;
 
   return (
-    <Component className={`${color(size)} text-gray-100 font-sans`}>
+    <Component
+      className={`${color(size)} text-gray-100 font-sans ${className}`}
+    >
       {children}
     </Component>
   );
